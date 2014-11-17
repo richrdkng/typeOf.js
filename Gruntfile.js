5dc032cef07026ae1bb5d8182805af313fecbbd2
Richard KnG <richrdkng@gmail.com>
2014-11-18 00:05:16 +0100

    Test Release 1.0.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,13 +176,13 @@ module.exports = function(grunt) {
         },
         coveralls: {
             options: {
-                // LCOV coverage file relevant to every target
-                src: coverageDir + "/lcov.info",
-
                 // When true, grunt-coveralls will only print a warning rather than
                 // an error, to prevent CI builds from failing unnecessarily (e.g. if
                 // coveralls.io is down). Optional, defaults to false.
                 force: false
+            },
+            main: {
+                src: coverageDir + "/lcov.info"
             }
         },
 
